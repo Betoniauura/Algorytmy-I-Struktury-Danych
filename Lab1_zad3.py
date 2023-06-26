685c9c2b7d02c2262f2e09bb8fc1f41eef345e8c
Betoniauura <115713926+Betoniauura@users.noreply.github.com>
2023-06-26 03:11:24 +0200

    Delete Lab1_zad3.py

--- Lab1_zad3.py
+++
@@ -1,9 +0,0 @@
-#Zadanie 3
-
-a = float(input("Wprowadź liczbę całkowitą dodatnią:"))
-print("Szczęśliwe numerki to: 0 1 2 3 4 5")
-x = [0,1,2,3,4,5]
-if a in x:
-    print("Trafiłeś")
-else:
-    print("Nie tym razem")
